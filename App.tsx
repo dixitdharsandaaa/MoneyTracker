445e76e9e9528156af0bb081ff2e283f8cd2c31b
dixitdharsandaaa <dixitdharsanda42@gmail.com>
2024-11-16 21:14:46 -0800

    setup project
    setup project

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello, TypeScript!</Text>
+    </View>
+  )
+}
+
+export default App
